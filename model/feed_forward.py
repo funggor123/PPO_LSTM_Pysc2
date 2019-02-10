6e08c6acd859acc8292317af7f1b0278f5e35cd9
s31b18 <funghiantie@gmail.com>
2019-02-10 21:34:30 +0800

    454

--- model/feed_forward.py
+++ model/feed_forward.py
@@ -10,66 +10,37 @@ class Model:
         self.obs_dimension = obs_dimension
         self.a_dimension = a_dimension
         self.is_continuous = is_continuous
-        self.policy_dense_unit = 100
-        self.critic_dense_unit = 100
+        self.num_unit = 100
 
-    def discrete_policy_output_layer(self, fc1, train, init):
-        return layers.dense(fc1, units=2, activation=tf.nn.softmax, kernel_initializer=init, trainable=train)
+    def discrete_policy_output_layer(self, fc1, train):
+        return layers.dense(fc1, units=2, activation=tf.nn.softmax, trainable=train)
 
-    def continuous_policy_output_layer(self, fc1, train, init):
-        sigma = layers.dense(fc1, units=1, activation=tf.nn.softplus,
-                             trainable=train) + 1e-4
-        mu = layers.dense(fc1, units=1, activation=tf.nn.tanh,
-                          trainable=train) * self.a_bound[1]
-        policy_out = tf.contrib.distributions.Normal(loc=mu, scale=sigma)
+    def continuous_policy_output_layer(self, fc1, train):
+        log_sigma = tf.get_variable(name="pi_sigma", shape=1, initializer=tf.zeros_initializer(), trainable=train)
+        bound = (self.a_bound[1] - self.a_bound[0]) / 2
+        mu = tf.layers.dense(fc1, units=1, activation=tf.nn.tanh, trainable=train) * bound
+        policy_out = tf.contrib.distributions.Normal(loc=mu, scale=tf.maximum(tf.exp(log_sigma), 0.0))
         return policy_out
 
-    def value_output_layer(self, fc1, train, init):
-        value_out = layers.dense(fc1, units=1, kernel_initializer=init, trainable=train)
+    def value_output_layer(self, fc1, train):
+        value_out = layers.dense(fc1, units=1, trainable=train)
         return value_out
 
     def make_actor_network(self, input_opr, name, train=True):
-        w_init = tf.random_normal_initializer(0., .1)
         with tf.variable_scope(name):
-            fc1 = layers.dense(input_opr, units=self.policy_dense_unit, activation=tf.nn.relu6,
-                                     trainable=train, kernel_initializer=w_init)
+            fc1 = layers.dense(input_opr, units=self.num_unit, activation=tf.nn.relu6,
+                               trainable=train)
             if self.is_continuous:
-                policy_out = self.continuous_policy_output_layer(fc1, train, None)
+                policy_out = self.continuous_policy_output_layer(fc1, train)
             else:
-                policy_out = self.discrete_policy_output_layer(fc1, train, None)
+                policy_out = self.discrete_policy_output_layer(fc1, train)
 
         params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
         return policy_out, params
 
     def make_critic_network(self, input_opr, name, train=True):
-        w_init = tf.random_normal_initializer(0., .1)
         with tf.variable_scope(name):
-            fc1 = layers.dense(input_opr, units=self.critic_dense_unit, activation=tf.nn.relu6, kernel_initializer=w_init, trainable=train)
-            value_out = self.value_output_layer(fc1, train, None)
+            fc1 = layers.dense(input_opr, units=self.num_unit, activation=tf.nn.relu6, trainable=train)
+            value_out = self.value_output_layer(fc1, train)
         params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
         return value_out, params
-
-
-    '''
-        def make_network(self, input_opr, name, train=True):
-        with tf.variable_scope(name):
-            init = tf.random_normal_initializer(0., .1)
-            dense_out = layers.dense(input_opr, units=self.num_units, activation=self.activation,
-                                     kernel_initializer=init,
-                                     trainable=train)
-            for n in range(0, self.num_layers - 1):
-                dense_out = layers.dense(dense_out, units=self.num_units, activation=self.activation,
-                                         kernel_initializer=init, trainable=train)
-            if self.is_continuous:
-                policy_out = self.continous_output(dense_out, train, init)
-            else:
-                policy_out = self.discrete_output(dense_out, train, init)
-
-            value_out = self.value_output(dense_out, train, init)
-
-        params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
-
-        return value_out, policy_out, params
-    '''
-
-
