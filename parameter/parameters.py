765b95c5efaad0f6f64486bb9dddd2575172dce9
s31b18 <funghiantie@gmail.com>
2019-02-11 00:18:40 +0800

    %454

--- parameter/parameters.py
+++ parameter/parameters.py
@@ -4,6 +4,8 @@ from feature.gae import GAE
 from model.feed_forward import Model
 from algorithum.ppo import PPO
 from model.conv_net import ConvNet
+from algorithum.a2c import A2C
+
 
 def get_car_pole():
     env = Environment(discrete_action_bound=[2], observation_space_dimension=(4,), action_space_dimension=(1,),
@@ -26,7 +28,7 @@ def get_car_pole():
     return actor, env, train
 
 
-def get_pendulum():
+def get_pendulumPPO():
     env = Environment(discrete_action_bound=[2], observation_space_dimension=(3,), action_space_dimension=(1,),
                       is_continuous=True, gym_string="Pendulum-v0")
     feature_transform = GAE(obs_dimension=env.observation_space_dimension, a_dimension=env.action_space_dimension,
@@ -36,7 +38,27 @@ def get_pendulum():
                          a_bound=env.a_bound)
     actor = PPO(obs_dimension=env.observation_space_dimension, lr=0.0001, feature_transform=feature_transform,
                 model=feed_forward,
+                a_dimension=env.action_space_dimension,
+                action_space_length=env.discrete_action_bound,
+                regular_str=1e-2,
                 epsilon=0.1,
+                minibatch=32,
+                epoch=10
+                )
+    train = Train(train=True, max_episode=5e5, max_step=10000, batch_size=8192)
+    return actor, env, train
+
+
+def get_pendulumA2C():
+    env = Environment(discrete_action_bound=[2], observation_space_dimension=(3,), action_space_dimension=(1,),
+                      is_continuous=True, gym_string="Pendulum-v0")
+    feature_transform = GAE(obs_dimension=env.observation_space_dimension, a_dimension=env.action_space_dimension,
+                            gamma=0.90, beta=1, is_continuous=env.is_continuous)
+    feed_forward = Model(a_len=env.discrete_action_bound, a_dimension=env.action_space_dimension,
+                         obs_dimension=env.observation_space_dimension, is_continuous=env.is_continuous,
+                         a_bound=env.a_bound)
+    actor = A2C(obs_dimension=env.observation_space_dimension, lr=0.0001, feature_transform=feature_transform,
+                model=feed_forward,
                 a_dimension=env.action_space_dimension,
                 action_space_length=env.discrete_action_bound,
                 regular_str=1e-2,
@@ -46,14 +68,15 @@ def get_pendulum():
     train = Train(train=True, max_episode=5e5, max_step=10000, batch_size=8192)
     return actor, env, train
 
+
 def get_racing():
     env = Environment(discrete_action_bound=[2], observation_space_dimension=(96, 96, 3), action_space_dimension=(3,),
                       is_continuous=True, gym_string="CarRacing-v0")
     feature_transform = GAE(obs_dimension=env.observation_space_dimension, a_dimension=env.action_space_dimension,
                             gamma=0.90, beta=1, is_continuous=env.is_continuous)
     feed_forward = ConvNet(a_len=env.discrete_action_bound, a_dimension=env.action_space_dimension,
-                         obs_dimension=env.observation_space_dimension, is_continuous=env.is_continuous,
-                         a_bound=env.a_bound)
+                           obs_dimension=env.observation_space_dimension, is_continuous=env.is_continuous,
+                           a_bound=env.a_bound)
     actor = PPO(obs_dimension=env.observation_space_dimension, lr=0.0001, feature_transform=feature_transform,
                 model=feed_forward,
                 epsilon=0.1,
@@ -65,4 +88,3 @@ def get_racing():
                 )
     train = Train(train=True, max_episode=5e5, max_step=200, batch_size=32, print_every_episode=1)
     return actor, env, train
-
