6d3cb1d27f4444eefce9d8210abfeba88d17cc0e
s31b18 <funghiantie@gmail.com>
2019-03-16 18:02:14 +0800

    sef

--- parameter/parameters.py
+++ parameter/parameters.py
@@ -12,22 +12,22 @@ def get_car_pole():
     env = Environment(discrete_action_bound=[2], observation_space_dimension=(4,), action_space_dimension=(1,),
                       is_continuous=False, gym_string="CartPole-v0")
     feature_transform = GAE(obs_dimension=env.observation_space_dimension, a_dimension=env.action_space_dimension,
-                            gamma=0.95, beta=1, is_continuous=env.is_continuous)
+                            gamma=0.99, beta=1, is_continuous=env.is_continuous)
     feed_forward = Model(a_len=env.discrete_action_bound, a_dimension=env.action_space_dimension,
                          obs_dimension=env.observation_space_dimension, is_continuous=env.is_continuous,
                          a_bound=env.a_bound, is_cat=True)
     actor = PPO(obs_dimension=env.observation_space_dimension, lr=1e-4, feature_transform=feature_transform,
                 model=feed_forward,
-                epsilon=0.2,
+                epsilon=0.1,
                 a_dimension=env.action_space_dimension,
                 action_space_length=env.discrete_action_bound,
                 regular_str=1e-2,
-                minibatch=64,
+                minibatch=16,
                 vf_coef=1,
-                epoch=4,
+                epoch=10,
                 max_grad_norm=0.5,
                 )
-    train = Train(train=True, max_episode=1e5, max_step=10000, batch_size=200)
+    train = Train(train=True, max_episode=1e5, max_step=10000, batch_size=64)
     return actor, env, train
 
 
